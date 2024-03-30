e750164ec8fd9364611920d285cb46ec37e2d4f7
Chase Hockenberry <157448120+Chase-Hockenberry@users.noreply.github.com>
2024-03-29 22:24:35 -0500

    Added logic to the hand class and a discard function to help discard cards after each action. Also created evaluate strength function to evaluate the strength of all possible hands

--- five_card_poker.rb
+++ five_card_poker.rb
@@ -37,8 +37,36 @@ class Card
       end
     end
     class Hand
-
-    end
+        attr_accessor :cards
+  
+        def initialize(cards)
+          @cards = cards
+        end
+  
+        def to_s
+          @cards.map(&:to_s).join(', ')
+        end
+  
+        def discard(indices, deck)
+          indices.each do |index|
+            @cards[index] = deck.deal(1).first
+          end
+        end
+  
+        def evaluate_strength
+          case
+          when straight_flush? then [8, straight_flush_value]
+          when four_of_a_kind? then [7, four_of_a_kind_value]
+          when full_house? then [6, full_house_value]
+          when flush? then [5, flush_value]
+          when straight? then [4, straight_value]
+          when three_of_a_kind? then [3, three_of_a_kind_value]
+          when two_pair? then [2, two_pair_value]
+          when one_pair? then [1, one_pair_value]
+          else [0, high_card_value]
+          end
+        end
+        end
     class Player
 
     end
