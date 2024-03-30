773ccf1a6b714aaf33cc3592e6c2dfd5fd94724f
Chase Hockenberry <157448120+Chase-Hockenberry@users.noreply.github.com>
2024-03-29 22:21:20 -0500

    Added all suits and values to the deck and made a shuffle and deal function to help shuffle the deck after each action and deal a new set after each action

--- five_card_poker.rb
+++ five_card_poker.rb
@@ -12,7 +12,29 @@ class Card
   end
   
     class Deck
-        
+        SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades'].freeze
+        VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'].freeze
+  
+        attr_accessor :cards
+  
+        def initialize
+          @cards = []
+              SUITS.each do |suit|
+                VALUES.each do |value|
+                    @cards << Card.new(suit, value)
+                end
+              end
+          shuffle!
+        end
+  
+        def shuffle!
+          @cards.shuffle!
+        end
+
+        def deal(num)
+          @cards.pop(num)
+        end
+      end
     end
     class Hand
 
